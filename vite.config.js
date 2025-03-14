cde5b11f0c5155437065b0fa553c36afadbf731f
Nopparat <97354576+NopXx@users.noreply.github.com>
2025-03-14 15:56:05 +0700

    fix vite build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'public/assets/scss/style.scss'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'public/assets/scss/style.scss',
+                'resources/css/carousel.css',
+                'resources/css/department.css',
+                'resources/css/new.css',
+                'resources/css/tinymce-content.css',
+            ],
             refresh: true,
         }),
     ],
